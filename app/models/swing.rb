c39ffa39b965d3a85425f2f816557c0c4f69793d
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-03-14 23:43:38 +0900

    swingchange

--- app/models/swing.rb
+++ app/models/swing.rb
@@ -1,6 +1,7 @@
 class Swing < ApplicationRecord
   validates :wrong,:correct, presence: true, uniqueness: true
   after_create do
-
+    Story.tag_list.remove(self.wrong)
+    Story.tag_list << self.current
   end
 end
