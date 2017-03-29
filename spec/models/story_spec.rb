ac9b3dcf3cc6229794aa805f5c79ed86f6b4c3df
elzup <guild0105@gmail.com>
2017-03-30 01:27:07 +0900

    Test Story#bracket_check

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -95,11 +95,37 @@ describe Story do
     end
   end
 
-  describe '#bracket_words' do
-    it 'works' do
-      words = %w(tag1 tag2)
-      story = create(:story, :title => "hoge【#{words[0]}】fuga【#{words[1]}】")
-      expect(story.bracket_words).to contain_exactly *words
+  describe '#bracket_check' do
+    before do
+      @words = %w(word1 word2)
+      @title = "hoge【#{@words[0]}】fuga【#{@words[1]}】"
+      @story = create(:story, :title => @title)
+    end
+
+    it 'ヒットなし' do
+      @story.bracket_check
+      expect(Story.last.title).to eq(@title)
+      expect(Story.last).to @story
+    end
+
+    it 'Swing にヒット' do
+      swing = Swing.regist(@words[0], 'tag')
+      @story.bracket_check
+      expect(Story.last.title).to eq("hogefuga【#{@words[1]}】")
+      expect(Story.last.tag_list).to eq([swing.correct])
+    end
+
+    it 'Tag にヒット' do
+      create(:story).regist_tag(@words[1])
+      expect(Story.find(@story.id).title).to eq("hoge【#{@words[0]}】fuga")
+      expect(Story.find(@story.id).tag_list).to eq([@words[1]])
+    end
+
+    it '複数ヒット' do
+      swing = Swing.regist(@words[1], 'tag')
+      create(:story).regist_tag(@words[0])
+      expect(Story.find(@story.id).title).to eq("hogefuga")
+      expect(Story.find(@story.id).tag_list).to eq([@words[0], swing.correct])
     end
   end
 end
