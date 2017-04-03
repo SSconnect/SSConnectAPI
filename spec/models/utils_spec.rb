ac41acb06df5a548c89354ef979a5a2f3f23690a
elzup <guild0105@gmail.com>
2017-04-03 11:13:31 +0900

    Fix String#has_emoji

--- spec/models/utils_spec.rb
+++ spec/models/utils_spec.rb
@@ -4,8 +4,8 @@ describe String do
   describe '#has_emoji?' do
     it 'works' do
       expect('ほげ'.has_emoji?).to be_falsey
-      expect('😎'.has_emoji?).to be_truthy
-      expect(' マルチバイト 🐬 '.has_emoji?).to be_truthy
+      expect("a 🍰".has_emoji?).to be_truthy
+      expect(" マルチバイト 🐬 ".has_emoji?).to be_truthy
     end
   end
 end
