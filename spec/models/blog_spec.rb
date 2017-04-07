284e8805571fa2d3b63c526b71f6e3e906173602
elzup <guild0105@gmail.com>
2017-04-07 13:06:13 +0900

    Update annotate

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  url        :string
+#  rss        :string
+#  selector   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 describe Blog do
