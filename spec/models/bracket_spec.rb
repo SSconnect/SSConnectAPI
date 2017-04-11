4d408f6b17716bd14b27c437e7de928bc15062d4
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-04-12 02:54:20 +0900

    bracketのテストを書いた

--- spec/models/bracket_spec.rb
+++ spec/models/bracket_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Bracket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Bracket do
+  describe 'after create' do
+    it 'after create がきちんと行われている' do
+      Bracket.create(:title => 'tag1')
+      story = Story.regist_story('【tag1】test「」', %w(tag2))
+      title = story.title
+      tags = story.tag_list
+      new_title = Story.space_delete(Story.fix_title(title,tags))
+      story.rename_title(new_title)
+      expect(story.title).to eq ('test「」')
+    end
+  end
 end
