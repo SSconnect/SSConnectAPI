9fbbb238635c0bc394016d7076a2d8064d1021a0
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-04-17 15:31:22 +0900

    Article Entity

--- app/controllers/entity/article_entity.rb
+++ app/controllers/entity/article_entity.rb
@@ -2,8 +2,8 @@ module Entity
   class ArticleEntity < Grape::Entity
     expose :id
     expose :url
-    expose :blog,using: BlogEntity
     expose :created_at
     expose :updated_at
+    expose :blog,using: BlogEntity
   end
 end
